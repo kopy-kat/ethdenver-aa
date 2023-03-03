ad9e82d666bc11914e1f9acd26ec21408d0e9eeb
OliverT1 <oturnbull1@gmail.com>
2023-03-03 16:26:19 -0700

    Code to generate call data for adding a facet via diamond cut

---
+++ aa-blockchain/scripts/addFacet.js
@@ -0,0 +1,31 @@
+// get hardhat ethers
+// require("@nomiclabs/hardhat-ethers");
+
+const { getSelectors, FacetCutAction } = require('./libraries/diamond.js')
+
+
+async function createFacetAddCallData(facetAddress, facetName, diamondAddress) {
+    // const accounts = await ethers.getSigners()
+    // const contractOwner = accounts[0]
+    // create ethers Contract object for facet
+    const facet = await ethers.getContractAt(facetName, facetAddress)
+    // get selectors for facet
+    // deploy DiamondLoupeFacet
+    const cut = []
+    cut.push({
+        facetAddress: facet.address,
+        action: FacetCutAction.Add,
+        functionSelectors: getSelectors(facet)
+      })
+    console.log(getSelectors(facet))
+    // get the raw call date for call to diamondCut
+    const diamondCut = await ethers.getContractAt('DiamondCutFacet', diamondAddress)
+    const tx = await diamondCut.populateTransaction.diamondCut(cut, ethers.constants.AddressZero, '0x')
+    return tx.data
+
+
+}
+
+(async () => {
+    console.log(await createFacetAddCallData('0x5FD6eB55D12E759a21C09eF703fe0CBa1DC9d88D', 'DiamondLoupeFacet','0xcD6a42782d230D7c13A74ddec5dD140e55499Df9' ))
+  })()
